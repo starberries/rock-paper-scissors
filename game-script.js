134a32085b1a845f26c6d087069e5a9f22587934
Starberries <maistarberries@gmail.com>
2024-01-17 03:05:00 -0500

    Add display of round results
    Add a div to index.html and convert console.log lines to DOM commands
    in order to display the results of each round. Additionally, remove
    unnecessary remaining console.log commands to keep the code clean.

--- game-script.js
+++ game-script.js
@@ -11,52 +11,46 @@ const scissors = document.getElementById("scissors").addEventListener("click", (
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
     let i = Math.floor(Math.random() * 3);
-    let result = options[i]; 
-    console.log(result);
-    return result;
+    let choice = options[i];
+    return choice;
 };
 
 function getPlayerChoice(choice) {
-    // let choice = prompt("Rock, paper, or scissors?");
     let a = choice.charAt(0).toUpperCase();
     let b = choice.slice(1).toLowerCase();
     choice = a + b;
-    if (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") {
-        choice = console.log("Try again!")
-    } else {
-        console.log(choice);
-        return choice;
-    }
+    return choice;
 };
 
 function gameRound(playerSelection, computerSelection) {
     let result
+    let resultMessage = document.getElementById("results")
 
     if (playerSelection == computerSelection) {
         result = 0;
-        console.log(`${playerSelection} vs. ${computerSelection} is a tie!`);
+        resultMessage.textContent = `${playerSelection} vs. ${computerSelection} is a tie!`;
     }
     
     else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper")) {
         result = 1
-        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);    
+        resultMessage.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;    
     } 
     
     else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Rock")) {
         result = 0;
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
+        resultMessage.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
-    console.log(result);
     return result;
 };
 
 function game(playerChoice) {
     let wins = 0;
     let result = gameRound(getPlayerChoice(playerChoice), getComputerChoice());
+    let resultMessage = document.getElementById("results");
     if (result) {
         wins++;
     }
@@ -67,5 +61,5 @@ function game(playerChoice) {
     //         wins++;
     //     }
     // }
-    console.log(`You won ${wins} rounds!`);
+    resultMessage.textContent += (`You won ${wins} rounds!`);
 };
